a38d35ba9235fc49d9728913a03f0f1190be55b8
Joe Anderson <joseph.anderson423@gmail.com>
2018-09-25 11:43:39 -0600

    created function to assign an ID

--- app.js
+++ app.js
@@ -14,12 +14,42 @@ var budgetController = (function() {
         this.value = value;
     };
 
+    var data = {
 
-    var allExpenses = [];
-    var allIncomes = [];
-    var totalExpenses = 0;
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
 
-    
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            data.allItems[type].push(newItem);
+            return newItem;
+
+            
+        }
+    };
+
 })();
 
 
@@ -70,9 +100,11 @@ var controller = (function(budgetCtrl, UICtrl) {
     }
 
     var ctrlAddItem = function() {
+        var input, newItem;
         // 1. Get the filed input data.
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
         // 2. Add the item to the budget controller.
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the item to the UI.
         // 4. Calculate the budget.
         // 5. Display the budget on the UI.
